9986f6b262e3a102fe10c68527945689ae220ac0
nrkhlifah28 <94022360+nrkhlifah28@users.noreply.github.com>
2022-09-12 23:54:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ global.mods = JSON.parse(fs.readFileSync('./src/moderator.json')) // Want some h
 global.thumbt = global.img
 global.fsx = 999999999999
 global.kontak = [
-['16199961931', 'Ziv San', true], 
-['6282179137771', 'HAORI IQ-MD', true] 
+['6289618728196', 'Nur Khlfh', true], 
+['6287892870587', 'NoerBoTZ-NR', true] 
 ]
 global.APIs = { // API Prefix
   // name: 'https://website'
